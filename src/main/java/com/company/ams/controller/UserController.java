743b33ab4e6347ce593768dc4c5f16a5d30ddeae
Vaibhav Shukla (19198) <vaibhav.shukla02+pinegit@pinelabs.com>
2025-09-26 16:59:26 +0530

    Added DTO and Service classes

--- src/main/java/com/company/ams/controller/UserController.java
+++ src/main/java/com/company/ams/controller/UserController.java
@@ -1,8 +1,23 @@
 package com.company.ams.controller;
 
+import com.company.ams.dto.request.RegisterUserRequest;
+import com.company.ams.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/register")
+    public  String registerUser(@RequestBody RegisterUserRequest request) {
+
+     userService.registerUser(request);
+     return "List of users";
+    }
 }
